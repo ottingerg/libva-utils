08e759397aee7412ce097fd289e466f5aadb05fe
ottingerg <g.ottinger@gmx.at>
2018-05-24 22:40:12 +0200

    display current commandline

--- encode/test/runtests.sh
+++ encode/test/runtests.sh
@@ -10,6 +10,7 @@ fi
 OUTFILE=$8
 LOGFILE=log.txt
 
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7
 echo -n "FILE: ${OUTFILE} TEST 1 - CQP $4: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -19,6 +20,7 @@ else
   echo "PASS" >> ${LOGFILE}
 fi
 
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 2 - CQP $4 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -29,6 +31,7 @@ else
 fi
 
 
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 3 - CQP $4 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -41,6 +44,7 @@ fi
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct3_l012.ivf > ${OUTFILE}_cqp$4_svct3_l01.ivf
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct3_l01.ivf > ${OUTFILE}_cqp$4_svct3_l0.ivf
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct2_l01.ivf > ${OUTFILE}_cqp$4_svct2_l0.ivf
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7
 echo -n "FILE: ${OUTFILE} TEST 4 - CBR $5: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -49,6 +53,7 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 5 - CBR $5 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -57,6 +62,7 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 6 - CBR $5 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -68,6 +74,7 @@ fi
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct3_l012.ivf > ${OUTFILE}_cbr$5_svct3_l01.ivf
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct3_l01.ivf > ${OUTFILE}_cbr$5_svct3_l0.ivf
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct2_l01.ivf > ${OUTFILE}_cbr$5_svct2_l0.ivf
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 7 - VBR $5_$6: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -76,6 +83,7 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 8 - VBR $5_$6 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
@@ -84,6 +92,7 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
+echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7"
 ../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 9 - VBR $5_$6 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
