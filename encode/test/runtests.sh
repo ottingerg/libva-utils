62069de3a6d330d76f5f98a84e83e0e58809c72b
Georg Ottinger <g.ottinger@gmx.at>
2018-07-12 11:13:19 +0200

    test now accept encoder as argument

--- encode/test/runtests.sh
+++ encode/test/runtests.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# runtests w h inputfile quantizerparameter bitrate max_vbr_bitrate fps outfile
+# runtests w h inputfile quantizerparameter bitrate max_vbr_bitrate fps outfile encoder
 
-if [ $# -ne 8 ]
+if [ $# -ne 9 ]
 then
-echo "Usage: $0 Width Height INPUTFILE Quantizationparameter Bitrate MaxVBRBitrate FPS OUTFILE"
+echo "Usage: $0 Width Height INPUTFILE Quantizationparameter Bitrate MaxVBRBitrate FPS OUTFILE ENCODER"
 exit -1
 fi
 
+ENCODER=$9
 OUTFILE=$8
 LOGFILE=log.txt
 
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4.ivf --qp $4 -f $7
 echo -n "FILE: ${OUTFILE} TEST 1 - CQP $4: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -20,8 +21,8 @@ else
   echo "PASS" >> ${LOGFILE}
 fi
 
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4_svct3_l012.ivf --qp $4 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 2 - CQP $4 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -31,8 +32,8 @@ else
 fi
 
 
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cqp$4_svct2_l01.ivf --qp $4 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 3 - CQP $4 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -44,8 +45,8 @@ fi
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct3_l012.ivf > ${OUTFILE}_cqp$4_svct3_l01.ivf
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct3_l01.ivf > ${OUTFILE}_cqp$4_svct3_l0.ivf
 ./vp8halfrate < ${OUTFILE}_cqp$4_svct2_l01.ivf > ${OUTFILE}_cqp$4_svct2_l0.ivf
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5.ivf --fb $5 --rcmode 1 -f $7
 echo -n "FILE: ${OUTFILE} TEST 4 - CBR $5: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -53,8 +54,8 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5_svct3_l012.ivf --fb $5 --rcmode 1 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 5 - CBR $5 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -62,8 +63,8 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_cbr$5_svct2_l01.ivf --fb $5 --rcmode 1 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 6 - CBR $5 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -74,8 +75,8 @@ fi
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct3_l012.ivf > ${OUTFILE}_cbr$5_svct3_l01.ivf
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct3_l01.ivf > ${OUTFILE}_cbr$5_svct3_l0.ivf
 ./vp8halfrate < ${OUTFILE}_cbr$5_svct2_l01.ivf > ${OUTFILE}_cbr$5_svct2_l0.ivf
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6.ivf --fb $5 --vbr_max $6 --rcmode 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 7 - VBR $5_$6: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -83,8 +84,8 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct3_l012.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 3 --intra_period 32 -f $7
 echo -n "FILE: ${OUTFILE} TEST 8 - VBR $5_$6 SVCT 3: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
@@ -92,8 +93,8 @@ then
 else
   echo "PASS" >> ${LOGFILE}
 fi
-echo "../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7"
-../vp8enc $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7
+echo "${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7"
+${ENCODER} $1 $2 $3 ${OUTFILE}_vbr$5_$6_svct2_l01.ivf --fb $5 --vbr_max $6 --rcmode 2 --temp_svc 2 -f $7
 echo -n "FILE: ${OUTFILE} TEST 9 - VBR $5_$6 SVCT 2: " >> ${LOGFILE}
 if [ $? -ne 0 ]
 then
--- encode/test/runtestset.sh
+++ encode/test/runtestset.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 VIDEODIR=~/Videos/
-PLAYLIST=vp8enc_test_playlist.txt
+ENCODER=../vp8enc
+
+if [ ! -z "$1" ]
+  then
+    ENCODER=$1
+fi
+
+PLAYLIST=${ENCODER}_test_playlist.txt
 
 start=$(date +%s)
-./runtests.sh 352 288 ${VIDEODIR}akiyo_w352_h288_f300_fps30.yuv 26 350 500 30 akiyo_w352_h288_f300
-./runtests.sh 720 486 ${VIDEODIR}football_w720_h486_f360_fps30.yuv 26 700 1000 30 football_w720_h486_f360
-./runtests.sh 1280 720 ${VIDEODIR}stockholm_w1280_h720_f604_fps60.yuv 26 1400 2000 60 stockholm_w1280_h720_f604
-./runtests.sh 1920 1080 ${VIDEODIR}tractor_w1920_h1080_f690_fps25.yuv 26 2800 4000 25 tractor_w1920_h1080_f690
-./runtests.sh 4096 2160 ${VIDEODIR}crosswalk_w4096_h2160_f300_fps60.yuv 26 5600 8000 60 crosswalk_w4096_h2160_f300
+./runtests.sh 352 288 ${VIDEODIR}akiyo_w352_h288_f300_fps30.yuv 26 350 500 30 akiyo_w352_h288_f300 ${ENCODER}
+./runtests.sh 720 486 ${VIDEODIR}football_w720_h486_f360_fps30.yuv 26 700 1000 30 football_w720_h486_f360 ${ENCODER}
+./runtests.sh 1280 720 ${VIDEODIR}stockholm_w1280_h720_f604_fps60.yuv 26 1400 2000 60 stockholm_w1280_h720_f604 ${ENCODER}
+./runtests.sh 1920 1080 ${VIDEODIR}tractor_w1920_h1080_f690_fps25.yuv 26 2800 4000 25 tractor_w1920_h1080_f690 ${ENCODER}
+./runtests.sh 4096 2160 ${VIDEODIR}crosswalk_w4096_h2160_f300_fps60.yuv 26 5600 8000 60 crosswalk_w4096_h2160_f300 ${ENCODER}
 ls -1 *.ivf > ${PLAYLIST}
 end=$(date +%s)
 runtime=$((end-start))
